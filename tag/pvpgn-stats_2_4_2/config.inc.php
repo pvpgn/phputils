1dcb3ef3211a41b7a0d61af44fa69845fd1911f9
pelish <pelish@370ddefe-6ab7-4699-94d9-c3b9f74dbbdb>
2009-11-04 22:44:41 +0000

    pvpgn-stats 2.4.2 release

---
+++ tag/pvpgn-stats_2_4_2/config.inc.php
@@ -0,0 +1,140 @@
+<?php
+// ----------------------------------------------------------------------
+// Player -vs- Player Gaming Network Statistics System
+// http://www.stormzone.ru/
+// http://www.rino.com.co/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Original author: jfro (imeepmeep@hotmail.com)
+// Author from 2.3.20: Snaiperx (http://www.rino.com.co/)
+// Author from 2.3.16: STORM (http://www.stormzone.ru/)
+// Tanzania theme author: Tanzania (tanzania@gmx.net)
+// ----------------------------------------------------------------------
+
+
+
+// ---------------------------------------------------------------------------------
+// Database & System Config settings - Key
+//
+//      db_host:           MySQL Database Hostname
+//      db_database:       MySQL Database Name
+//      db_user:           MySQL Username
+//      db_pass:           MySQL Password
+//      db_prefix          MySQL Database Prefix
+//      db_record:         Name of the table with player records in your MySQL DB
+//      db_bnet:           Name of the table with player info`s in your MySQL DB
+//      db_profile:        Name of the table with player profile in your MySQL DB
+//      db_teams:          Name of the table with team records in your MySQL DB
+//      db_friend:         Name of the table with friends records in your MySQL DB
+//      db_counters:       Name of the table with counter records in your MySQL DB
+//      db_d2:             Name of the table with d2ladder records in your MySQL DB
+//
+// *NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*
+// *NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*
+// (Provided my Gambit)
+// For better support for everyone, I have added a central location to assign links for
+// all PHP and HTML pages.  Now you don't have to scour HTML code for links/URL's to wrong domains.
+// Assighn the values below to correct all links and refrences to your webpage.
+//
+//      (DO NOT UNCOMMENT, CHANGE BELOW UNDER "// System Config settings")
+//      homepage           Full URL to your websites Home Page
+//      ladderroot         Full URL to main stats page
+//      (DO NOT UNCOMMENT, CHANGE BELOW UNDER "// System Config settings")
+//
+// *NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*
+// *NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*NEW*
+
+// System Config settings
+$site_name = "PvPGN server";
+$db_host = "localhost";
+$db_database = "pvpgn";
+$db_user = "pvpgn";
+$db_pass = "pvpgnrocks";
+$db_prefix = "pvpgn_";
+$db_record = $db_prefix."Record";
+$db_bnet = $db_prefix."bnet";
+$db_profile = $db_prefix."profile";
+$db_teams = $db_prefix."team";
+$db_friend = $db_prefix."friend";
+$db_counters = $db_prefix."counters";
+$db_d2 = $db_prefix."d2ladder";
+$homepage = "http://my.server/";
+$ladderroot = "http://my.server/ladder/";
+
+
+$theme = "default";
+
+$use_php_xslt = true; // only change this if you know you have xsltproc installed and not php-xslt
+$xslt_command = "xsltproc"; // for when you don't have php4-xslt but have the xsltproc command or sabcmd
+
+// set pvpgn_dir to where you pvpgn directory is, include trailing slash
+$pvpgn_dir = "D:/pvpgn/";
+$ladders_dir = $pvpgn_dir."var/ladders/";
+
+$w3ladder_xsl_file = getcwd()."/themes/$theme/w3ladder.xsl";
+$d2ladder_xml_file = $ladders_dir."d2ladder.xml";
+$d2ladder_xsl_file = getcwd()."/themes/$theme/d2ladder.xsl";
+// ---------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------
+// PvPGN variables settings
+//
+//     icon_levelx:          Wins required for TFT icon
+//
+// ---------------------------------------------------------------------------------
+
+$icon_level1 = 25;
+$icon_level2 = 50;
+$icon_level3 = 100;
+$icon_level4 = 200;
+$icon_level5 = 500;
+
+// ---------------------------------------------------------------------------------
+// Page settings
+//
+//      site_theme:        Theme you want to run
+//      page_max:          How many players you want to display on one page
+//      default_game:      Defines stats for which game user will see on entering
+//      default_type:      Defines which type of game user will see on entering
+//      date_format:       Defines PHP date format used in stats displaying
+//      d2ladder_file:     Path on your system which shows ladder.D2DV location
+//      d2update_time:     Time to update your D2 DB (in seconds), 0 is off
+//      stats_version:     Don't change, just for easier version display
+//
+// ---------------------------------------------------------------------------------
+
+$site_theme = "bnet";
+$page_max = "50";
+$default_game = "W3XP";
+$default_type = "solo";
+$date_format = " F j - G:i";
+$d2ladder_file = "D:/pvpgn/var/ladders/ladder.D2DV";
+$d2update_time = "3600";
+$stats_version = "2.4.20";
+
+
+// ---------------------------------------------------------------------------------
+// Optional User file settings (unfinished, doesn`t work)
+//
+//      pvpgn_users:       Path on your system which shows user dir location
+//      use_files:         Defines whether we use files or database
+//
+// ---------------------------------------------------------------------------------
+
+$pvpgn_users = "/usr/local/pvpgn/var/users";
+$use_files = false;
+
+?>
---
+++ tag/pvpgn-stats_2_4_2/includes/mysql_handler.php
@@ -0,0 +1,86 @@
+<?php
+// ----------------------------------------------------------------------
+// Player -vs- Player Gaming Network Statistics System
+// http://www.stormzone.ru/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Original author: jfro (imeepmeep@hotmail.com)
+// Author from 2.3.20: Snaiperx (http://www.rino.com.co/)
+// Author from 2.3.16: STORM (http://www.stormzone.ru/)
+// Tanzania theme author: Tanzania (tanzania@gmx.net)
+// ----------------------------------------------------------------------
+
+class mysql_handle {
+	var $db_connection;
+
+// ----------------------------------------------------------------------------------------------
+// This is how we connect to DB
+// ----------------------------------------------------------------------------------------------
+
+	function db_connect($db_hostname, $db_username, $db_password, $db_name)
+	{
+		$this->db_connection=mysql_connect($db_hostname, $db_username, $db_password) 
+			OR die ("Connection Error to Server");
+		mysql_select_db($db_name)
+			OR die("Connection Error to Database");
+		return $this->db_connection;
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we get queries from DB
+// ----------------------------------------------------------------------------------------------
+
+	function db_query($db_query) {
+		$result = mysql_query($db_query,$this->db_connection)
+			or die("Query: $db_query  <br /> Error: ".mysql_error()."<br />");
+		return $result;
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we fetch DB
+// ----------------------------------------------------------------------------------------------
+
+	function db_fetch($db_result) {
+		while($value = mysql_fetch_assoc($db_result)) {
+			$results[] = $value;
+		}
+		return $results;
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we disconnect from DB
+// ----------------------------------------------------------------------------------------------
+
+	function db_disconnect()
+	{
+		mysql_close($this->db_connection);
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we do search in DB
+// ----------------------------------------------------------------------------------------------
+
+	function db_search($value,$column,$column_want,$table) {
+		$query = "SELECT $column_want FROM $table WHERE `$column` LIKE '$value'";
+		$result = mysql_query($query,$this->db_connection);
+		while($entry = mysql_fetch_assoc($result)) {
+			$results[] = $entry;
+		}
+		return $results;
+	}
+}
+
+?>
