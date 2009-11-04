bdd8409d3dc17492f6e735b3f625c8c734f58d6c
pelish <pelish@370ddefe-6ab7-4699-94d9-c3b9f74dbbdb>
2009-11-04 23:13:23 +0000

    pvpgn-stats 2.4.5 release

---
+++ tag/pvpgn-stats_2_4_5/includes/page_handler.php
@@ -0,0 +1,104 @@
+<?php
+// ----------------------------------------------------------------------
+// Player -vs- Player Gaming Network Statistics System
+// http://pvpgn.spfree.net/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Author from 2.4.4: Pelish (pelish@gmail.com)
+// Original author: jfro (imeepmeep@hotmail.com)
+// Author from 2.3.20: Snaiperx (http://www.rino.com.co/)
+// Author from 2.3.16: STORM (http://www.stormzone.ru/)
+// Tanzania theme author: Tanzania (tanzania@gmx.net)
+// ----------------------------------------------------------------------
+
+class page_handle {
+	var $full_content;
+	var $page_data;
+	var $main_file;
+	var $theme;
+
+// ----------------------------------------------------------------------------------------------
+// This is how we setup the page
+// ----------------------------------------------------------------------------------------------
+
+	function setup($theme,$main_file,$page_data) {
+		$this->page_data = $page_data;
+		$this->main_file = $main_file;
+		$this->theme = $theme;
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we fetch the content
+// ----------------------------------------------------------------------------------------------
+
+	function fetch() {
+		return $this->full_content;	
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we parse:Change [[]] variables to real info in stats layout
+// ----------------------------------------------------------------------------------------------
+
+	function parse() {
+		$fp = fopen("themes/".$this->theme."/".$this->main_file,"r");
+		$main_content = fread($fp,filesize("themes/".$this->theme."/".$this->main_file));
+		$done = false;
+		do {
+			$pos = strpos($main_content,"[[");
+			$pos2 = strpos($main_content,"]]");
+			$key = substr($main_content,$pos+2,$pos2-$pos-2);
+			if(is_int($pos)&&is_int($pos2)) {
+				if(is_array($this->page_data[$key])) {
+					$main_content = str_replace("[[$key]]",$this->build_repeat($this->page_data[$key]),$main_content);
+				}
+				else { 
+					$main_content = str_replace("[[$key]]",$this->page_data[$key],$main_content);
+				}
+			}
+			else
+				$done = true;
+		} while(!$done);
+		$this->full_content = $main_content;
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we build
+// ----------------------------------------------------------------------------------------------
+
+	function build_repeat($data) {
+		$fp = fopen("themes/".$this->theme."/".$data['theme_file'],"r");
+		$size = filesize("themes/".$this->theme."/".$data['theme_file']);
+		$content = fread($fp,$size);
+		for($n=0;$n<count($data)-1;$n++) {
+			$temp = $content;
+			$done = false;
+			do {
+				$pos = strpos($temp,"[[");
+				$pos2 = strpos($temp,"]]");
+				$key = substr($temp,$pos+2,$pos2-$pos-2);
+				if(is_int($pos)&&is_int($pos2)) {
+					if ($data[$n][$key]=='') $temp = str_replace("[[".$key."]]",$this->page_data[$key],$temp);
+					else $temp = str_replace("[[".$key."]]",$data[$n][$key],$temp);
+				}
+				else$done = true;
+			} while(!$done);
+			$output .= $temp;
+		}
+		return $output;
+	}
+}
+
+?>
---
+++ tag/pvpgn-stats_2_4_5/includes/pvpgn_rank.php
@@ -0,0 +1,97 @@
+<?php
+// ----------------------------------------------------------------------
+// Player -vs- Player Gaming Network Statistics System
+// http://pvpgn.spfree.net/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Author from 2.4.4: Pelish (pelish@gmail.com)
+// Original author: jfro (imeepmeep@hotmail.com)
+// Author from 2.3.20: Snaiperx (http://www.rino.com.co/)
+// Author from 2.3.16: STORM (http://www.stormzone.ru/)
+// Tanzania theme author: Tanzania (tanzania@gmx.net)
+// ----------------------------------------------------------------------
+
+include_once("includes/mysql_handler.php");
+
+class pvpgn_rank {
+	var $db;
+
+// ----------------------------------------------------------------------------------------------
+// Defines connection to DB
+// ----------------------------------------------------------------------------------------------
+
+	function pvpgn_rank() {
+		global $db_host,$db_user,$db_pass,$db_database;
+		$this->db = new sql_handle();
+		$this->db->db_connect($db_host,$db_user,$db_pass,$db_database);
+	}
+
+// ----------------------------------------------------------------------------------------------
+// This is how we update ranks in the DB
+// ----------------------------------------------------------------------------------------------
+
+	function update_ranks($game,$type) {
+		global $db_record,$max_rank,$db_d2;
+		if(strstr($game,"WAR3" || strstr($game,"W3XP"))) {
+			if($type == "0")
+				$type = $game."_".$type;
+			$query = "SELECT ".$type."_xp,uid FROM `$db_record` WHERE not (`".$type."_xp`= 0) AND uid != 0 ORDER BY ".$type."_xp DESC LIMIT $max_rank";
+			$users = $this->db->db_query_fetch($query);
+			for($n=1;$n<count($users)+1;$n++) {
+				if($users[$n-1]['uid'] != 0) {
+					$query = "UPDATE `$db_record`	SET `".$type."_rank`=$n WHERE `uid`=".$users[$n-1]['uid'];
+					$this->db->db_query($query);
+				}
+			}
+		}
+		else if(strstr($game,"SEXP") || strstr($game,"STAR") || strstr($game,"W2BN")) {
+			if($type == "0") {
+				$type = $game."_".$type;
+				$query = "SELECT ".$type."_wins,uid FROM `$db_record` WHERE not(`".$type."_wins` is null) AND uid != 0 ORDER BY ".$type."_wins DESC LIMIT $max_rank";
+				$users = $this->db->db_query_fetch($query);
+				for($n=1;$n<count($users)+1;$n++) {
+					if($users[$n-1]['uid'] != 0) {
+						$query = "UPDATE `$db_record`	SET `".$type."_rank`=$n WHERE `uid`=".$users[$n-1]['uid'];
+						$this->db->db_query($query);
+					}
+				}
+			}	
+		}
+		else if(strstr($game,"DRTL")) {
+			if($type == "0") {
+				$type = $game."_".$type;
+				$query = "SELECT ".$type."_level,uid FROM `$db_record` WHERE not(`".$type."_level` is null) AND uid != 0 ORDER BY ".$type."_level DESC LIMIT $max_rank";
+				$users = $this->db->db_query_fetch($query);
+				for($n=1;$n<count($users)+1;$n++) {
+					if($users[$n-1]['uid'] != 0) {
+						$query = "UPDATE `$db_record`	SET `".$type."_rank`=$n WHERE `uid`=".$users[$n-1]['uid'];
+						$this->db->db_query($query);
+					}
+				}
+			}
+		}
+		else if(strstr($game,"D2DV") || strstr($game,"D2XP")) {
+			$query = "SELECT experience,charname FROM `$db_d2` WHERE `game`='$game' AND `type`='$type' ORDER BY experience DESC LIMIT $max_rank";
+			$users = $this->db->db_query_fetch($query);
+			for($n=1;$n<count($users)+1;$n++) {
+				$query = "UPDATE `$db_d2`	SET `rank`=$n WHERE `charname`='".$users[$n-1]['charname']."'";
+				$this->db->db_query($query);
+			}
+		}
+	}
+}
+
+?>
