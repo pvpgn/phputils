13e5e4d684d56f358ec2e8b7c806208334f63c97
pelish <pelish@370ddefe-6ab7-4699-94d9-c3b9f74dbbdb>
2009-11-04 23:23:42 +0000

    changes according to pvpgn-stats 2.4.6 release (thanks goes to Karel_II)

--- pvpgn-stats/SQL Files/STATS.sql
+++ pvpgn-stats/SQL Files/STATS.sql
@@ -1,23 +1,34 @@
-#------------------------------------------------------
-# Table structure PvPGN Statistics System v2.4.5
-#------------------------------------------------------
+##
+## Table structure for table 'd2ladder'
+##
 
-#
-# Table structure for table 'Record'
-#
 
-CREATE TABLE Record (
-  uid int(11) NOT NULL default '0',
-  SEXP_0_rank int(11) default '0',
-  STAR_0_rank int(11) default '0',
-  W2BN_0_rank int(11) default '0',
-  DRTL_0_rank int(11) default '0',
-  PRIMARY KEY  (uid)
+--%PREFIX%-- The same prefix must be set in config.inc.php
+
+
+CREATE TABLE `--%PREFIX%--d2ladder` (
+  `charname` varchar(16) NOT NULL default '',
+  `title` varchar(16) default NULL,
+  `level` int(4) default '1',
+  `class` varchar(10) default NULL,
+  `experience` float(8,0) default NULL,
+  `rank` int(11) default NULL,
+  `type` char(2) default 'SC',
+  `dead` varchar(5) default 'ALIVE',
+  `game` varchar(4) NOT NULL default '',
+  PRIMARY KEY  (`charname`),
+  KEY `experience` (`experience`)
 ) TYPE=MyISAM;
 
+CREATE TABLE `PvPGN`.`counters` (
+  `d2ladder_time` int( 11 ) NOT NULL default '0'
+) ENGINE = MYISAM DEFAULT CHARSET = latin1;
 
-#
-# Dumping data for table 'Record'
-#
+##
+## Table change for table 'Record'
+##
 
-INSERT INTO record VALUES("0", "0", "0", "0", "0");
+ALTER TABLE --%PREFIF%--Record ADD `SEXP_0_rank` INT DEFAULT '0' NOT NULL;
+ALTER TABLE --%PREFIF%--Record ADD `STAR_0_rank` INT DEFAULT '0' NOT NULL;
+ALTER TABLE --%PREFIF%--Record ADD `W2BN_0_rank` INT DEFAULT '0' NOT NULL;
+ALTER TABLE --%PREFIF%--Record ADD `DRTL_0_rank` INT DEFAULT '0' NOT NULL;
